ddf47b54ae447f4fb748bd965a4737c6a1b1f07e
rpanteleychuk <r.panteleychuk@gmail.com>
2019-05-07 10:14:34 +0300

    add first section markup

--- wp-content/themes/f5test-child/home.php
+++ wp-content/themes/f5test-child/home.php
@@ -8,6 +8,69 @@ Template Post Type: page
 
 
 <div class="main-wrapper">
+    <?php
+
+    if( have_rows('home_page') ):
+
+        while( have_rows('home_page') ) : the_row();
+
+            // get layout
+            $layout = get_row_layout();
+
+
+            // layout_1
+            if( $layout === 'first_section' ): ?>
+
+                <div class="section-1" style="background: url(<?php the_sub_field('background_image'); ?>) no-repeat center;  background-size: cover;">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-11 col-md-5 head-logo">
+                                <?php
+
+                                $image = get_sub_field('heading_logo');
+
+                                if (!empty($image)): ?>
+
+                                    <img class="img-fluid" src="<?php echo $image['url']; ?>"
+                                         alt="<?php echo $image['alt']; ?>"/>
+
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="col-11 col-md-5">
+                                <h2 class="text-center text-white text-uppercase"><?php the_sub_field('heading'); ?></h2>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="col-11 col-md-7">
+                                <p class="text-white"><?php the_sub_field('main_text'); ?></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+            <?php // layout_2
+            elseif( $layout === 'layout_2' ): ?>
+
+                <div class="section-2">
+                    <p><?php the_sub_field('sub_field_2'); ?></p>
+                </div>
+
+            <?php endif;
+
+        endwhile;
+
+    endif;
+
+    ?>
+
+
 
     <div class="section-1">
         <div class="container">привет</div>
