e04f552365019e92e3ad63d3624bd92e8684debb
rpanteleychuk <r.panteleychuk@gmail.com>
2019-05-07 13:29:57 +0300

    add third section markup

--- wp-content/themes/f5test-child/home.php
+++ wp-content/themes/f5test-child/home.php
@@ -24,7 +24,7 @@ Template Post Type: page
                 <div class="section-1 py-5" style="background: url(<?php the_sub_field('background_image'); ?>) no-repeat center;  background-size: cover;">
                     <div class="container">
                         <div class="row justify-content-center my-5">
-                            <div class="col-11 col-md-5 head-logo">
+                            <div class="col head-logo text-center">
                                 <?php
 
                                 $image = get_sub_field('heading_logo');
@@ -38,12 +38,12 @@ Template Post Type: page
                             </div>
                         </div>
                         <div class="row justify-content-center my-4">
-                            <div class="col-11 col-md-6 pt-4">
+                            <div class="col pt-4">
                                 <h2 class="text-center text-white text-uppercase main-heading-font"><?php the_sub_field('heading'); ?></h2>
                             </div>
                         </div>
                         <div class="row justify-content-center my-4">
-                            <div class="col-11 col-md-7">
+                            <div class="col col-md-7">
                                 <p class="text-white description-font"><?php the_sub_field('main_text'); ?></p>
                             </div>
                         </div>
@@ -57,12 +57,12 @@ Template Post Type: page
                 <div class="section-2 py-5">
                     <div class="container">
                         <div class="row justify-content-center">
-                            <div class="col-11 col-md-6">
+                            <div class="col">
                                 <h4 class="text-center second-section-heading-font"><?php the_sub_field('heading'); ?></h4>
                             </div>
                         </div>
                         <div class="row justify-content-center">
-                            <div class="col-11 col-md-7 my-4 text-center">
+                            <div class="col my-4 text-center">
                                 <?php
                                 $link = get_sub_field('button_link');
                                 if ($link):
@@ -76,11 +76,90 @@ Template Post Type: page
                             </div>
                         </div>
                         <div class="row justify-content-center mt-3">
-                            <h6 class="col-11 col-md-6 text-center second-section-subheading-font mb-0"><?php the_sub_field('sub_heading'); ?></h6>
-                            <p class="col-11 col-md-9 text-center second-section-text-font"><?php the_sub_field('text'); ?></p>
+                            <h6 class="col-12 col-md-6 text-center second-section-subheading-font mb-0"><?php the_sub_field('sub_heading'); ?></h6>
+                            <p class="col-12 col-md-9 text-center second-section-text-font"><?php the_sub_field('text'); ?></p>
                         </div>
                     </div>
                 </div>
+
+
+            <?php
+            elseif( $layout === 'third_section' ): ?>
+
+                <div class="section-3 py-5" style="background: url(<?php the_sub_field('background_image'); ?>) no-repeat center;  background-size: cover;">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <h3 class="heading-font text-white text-center"><?php the_sub_field('heading'); ?></h3>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <p class="text-white text-center"><?php the_sub_field('sub_heading'); ?></p>
+                            </div>
+                        </div>
+                        <div class="row row-flex justify-content-around">
+                            <div class="col-4">
+                                <div class="third-section-item py-3 pl-2 pr-1">
+                                    <?php
+
+                                    $image = get_sub_field('first_image');
+
+                                    if (!empty($image)): ?>
+
+                                        <img class="img-fluid d-inline" src="<?php echo $image['url']; ?>"
+                                             alt="<?php echo $image['alt']; ?>"/>
+
+                                    <?php endif; ?>
+                                    <h6 class="d-inline"><?php the_sub_field('first_item_heading'); ?></h6>
+                                    <p><?php the_sub_field('first_item_text'); ?></p>
+                                    <p><?php the_sub_field('first_item_subtext'); ?></p>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="third-section-item">
+                                    <?php
+
+                                    $image = get_sub_field('second_image');
+
+                                    if (!empty($image)): ?>
+
+                                        <img class="img-fluid d-inline" src="<?php echo $image['url']; ?>"
+                                             alt="<?php echo $image['alt']; ?>"/>
+
+                                    <?php endif; ?>
+                                    <h6 class="d-inline"><?php the_sub_field('second_item_heading'); ?></h6>
+                                    <p><?php the_sub_field('second_item_text'); ?></p>
+                                    <p><?php the_sub_field('second_item_subtext'); ?></p>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="third-section-item">
+                                    <?php
+
+                                    $image = get_sub_field('third_image');
+
+                                    if (!empty($image)): ?>
+
+                                        <img class="img-fluid d-inline" src="<?php echo $image['url']; ?>"
+                                             alt="<?php echo $image['alt']; ?>"/>
+
+                                    <?php endif; ?>
+                                    <h6 class="d-inline"><?php the_sub_field('third_item_heading'); ?></h6>
+                                    <p><?php the_sub_field('third_item_text'); ?></p>
+                                    <p><?php the_sub_field('third_item_subtext'); ?></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+            
+
+
 
 
             <?php // layout_2
@@ -89,6 +168,8 @@ Template Post Type: page
                 <div class="section-2">
                     <p><?php the_sub_field('sub_field_2'); ?></p>
                 </div>
+
+
 
             <?php endif;
 
