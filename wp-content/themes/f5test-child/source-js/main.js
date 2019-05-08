f14c2eb3a9aacb4e6645bcc1f442e71f42a8ba77
rpanteleychuk <r.panteleychuk@gmail.com>
2019-05-08 14:54:02 +0300

    edit scroll

--- wp-content/themes/f5test-child/source-js/main.js
+++ wp-content/themes/f5test-child/source-js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     // плавное перемещение страницы к нужному блоку
     $("a").click(function () {
         elementClick = $(this).attr("href");
-        destination = $(elementClick).offset().top;
+        destination = $(elementClick).offset().top -50;
         $("body,html").animate({scrollTop: destination }, 800);
     });
 });
