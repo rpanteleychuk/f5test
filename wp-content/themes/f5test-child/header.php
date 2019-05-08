6d299ae7478b0d068ea5d025377d5147403ee057
rpanteleychuk <r.panteleychuk@gmail.com>
2019-05-08 15:02:41 +0300

    edit code style

--- wp-content/themes/f5test-child/header.php
+++ wp-content/themes/f5test-child/header.php
@@ -13,37 +13,41 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Quattrocento+Sans:400,400i,700,700i|Quattrocento:400,700" rel="stylesheet">
-    
+    <link href="https://fonts.googleapis.com/css?family=Quattrocento+Sans:400,400i,700,700i|Quattrocento:400,700"
+          rel="stylesheet">
+
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 
-	<header id="masthead" class="site-header">
+    <header id="masthead" class="site-header">
         <div class="container">
             <div class="row pt-3">
                 <div class="col-12 col-md-6">
                     <div class="site-branding">
                         <?php
                         the_custom_logo();
-                        if ( is_front_page() && is_home() ) :
+                        if (is_front_page() && is_home()) :
                             ?>
-                            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                            <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                                      rel="home"><?php bloginfo('name'); ?></a></h1>
                         <?php
                         else :
                             ?>
-                            <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                            <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                                     rel="home"><?php bloginfo('name'); ?></a></p>
                         <?php
                         endif;
-                        $f5test_description = get_bloginfo( 'description', 'display' );
-                        if ( $f5test_description || is_customize_preview() ) :
+                        $f5test_description = get_bloginfo('description', 'display');
+                        if ($f5test_description || is_customize_preview()) :
                             ?>
                             <p class="site-description"><?php echo $f5test_description; /* WPCS: xss ok. */ ?></p>
                         <?php endif; ?>
@@ -53,24 +57,26 @@
                     <a href="tel:+1234567890"><i class="fas fa-phone"></i> Call Us<span>55 55 55 55</span></a>
                 </div>
                 <div class="col-12 col-md-3 link-mail">
-                    <a href="mailto:sales@ldtool.com"><i class="fas fa-envelope"></i> Email Us<span>sales@ldtool.com</span></a>
+                    <a href="mailto:sales@ldtool.com"><i class="fas fa-envelope"></i> Email
+                        Us<span>sales@ldtool.com</span></a>
                 </div>
             </div>
         </div>
     </header><!-- #masthead -->
-    <div class="menu-container test" id="navbar">
+    <div class="menu-container" id="navbar">
         <div class="container">
             <div class="d-flex">
                 <nav id="site-navigation" class="main-navigation">
                     <?php
-                    wp_nav_menu( array(
+                    wp_nav_menu(array(
                         'theme_location' => 'menu-1',
-                        'menu_id'        => 'primary-menu',
-                    ) );
+                        'menu_id' => 'primary-menu',
+                    ));
                     ?>
                 </nav><!-- #site-navigation -->
                 <a href="#get-quote" class="button-footer pt-3 d-none d-sm-none d-md-block">GET A QUOTE</a>
             </div>
         </div>
     </div>
-	<div id="content" class="site-content">
+
+    <div id="content" class="site-content">
