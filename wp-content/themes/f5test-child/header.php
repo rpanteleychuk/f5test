945065426351b70c5099d64d0f031de560fdd11f
rpanteleychuk <r.panteleychuk@gmail.com>
2019-05-08 13:06:40 +0300

    add header logo style and email + phone link

--- wp-content/themes/f5test-child/header.php
+++ wp-content/themes/f5test-child/header.php
@@ -17,6 +17,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Quattrocento+Sans:400,400i,700,700i|Quattrocento:400,700" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
@@ -25,27 +26,40 @@
 <div id="page" class="site">
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$f5test_description = get_bloginfo( 'description', 'display' );
-			if ( $f5test_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $f5test_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+        <div class="container">
+            <div class="row py-3">
+                <div class="col-12 col-md-6">
+                    <div class="site-branding">
+                        <?php
+                        the_custom_logo();
+                        if ( is_front_page() && is_home() ) :
+                            ?>
+                            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                        <?php
+                        else :
+                            ?>
+                            <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                        <?php
+                        endif;
+                        $f5test_description = get_bloginfo( 'description', 'display' );
+                        if ( $f5test_description || is_customize_preview() ) :
+                            ?>
+                            <p class="site-description"><?php echo $f5test_description; /* WPCS: xss ok. */ ?></p>
+                        <?php endif; ?>
+                    </div><!-- .site-branding -->
+                </div>
+                <div class="col-6 col-md-3 link-tele">
+                    <a href="tel:+1234567890"><i class="fas fa-phone"></i> Call Us<span>55 55 55 55</span></a>
+                </div>
+                <div class="col-6 col-md-3 link-mail">
+                    <a href="mailto:sales@ldtool.com"><i class="fas fa-envelope"></i> Email Us<span>sales@ldtool.com</span></a>
+                </div>
+            </div>
+        </div>
+
+
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'f5test' ); ?></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
@@ -53,6 +67,9 @@
 			) );
 			?>
 		</nav><!-- #site-navigation -->
+
+
+
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
