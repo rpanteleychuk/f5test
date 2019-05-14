9fd6d85836b165641a942e45d51e63bb15e8fe2c
rpanteleychuk <r.panteleychuk@gmail.com>
2019-05-14 10:27:15 +0300

    edit scroll offset

--- wp-content/themes/f5test-child/source-js/main.js
+++ wp-content/themes/f5test-child/source-js/main.js
@@ -1,5 +1,5 @@
 $("[href^='#']").click(function() {
-    var idtop = $($(this).attr("href")).offset().top;
+    var idtop = $($(this).attr("href")).offset().top -50;
     $('html,body').animate(
         // Time animation
         {scrollTop: idtop}, 800);
